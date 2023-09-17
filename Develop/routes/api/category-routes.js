13529d344cd0195c23f35b824944f1a71e4ce7ac
Ryan Mattson <ryan.mattson32@gmail.com>
2023-09-17 14:53:50 -0600

    Added second get route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -15,8 +15,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    // Find the category with the matching ID, including its associated products
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product }] });
+
+    // If the category is not found, send a 404 status with a custom message
+    if (!category) {
+      res.status(404).json({ message: 'id not found' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    // Handle errors by sending a 500 status with a custom message
+    res.status(500).json({ message: 'not found!' });
+  }
 });
 
 router.post('/', (req, res) => {
