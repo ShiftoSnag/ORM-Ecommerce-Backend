bd4c47ca007d24415f44fe89f3f7acc0b9591f1f
Ryan Mattson <ryan.mattson32@gmail.com>
2023-09-17 15:02:12 -0600

    Added get route 1 into product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,8 +5,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Products not found!" });
+  }
 });
 
 // get one product
